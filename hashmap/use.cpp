2f1f9645b6556757ba6603510368d5768b77a3ea
akm-akm <adityanick08@gmail.com>
2023-03-13 01:55:29 +0530

    ok

--- hashmap/use.cpp
+++ hashmap/use.cpp
@@ -7,6 +7,23 @@ int main(){
 
     pair <string,int> a("hello",1);
     m.insert(a);
+    pair <string,int> p = make_pair("how",2);
+    m.insert(p);
     cout << m.at("hello") << endl;
-    return 0;
+    cout << m.at("how") << endl;
+    m["how"] = 4;
+    cout << m.at("how") << endl;
+
+    cout << m["ok"] << endl;
+    cout << m.at("ok") << endl;
+    for(auto x:m){
+        cout<<x.first<<" "<<x.second<<endl;
+        
+    }
+    unordered_map<string, int> :: iterator i = m.begin();
+    while(i!=m.end()){
+        cout << i->first << i->second << endl;
+        i++;
+    }
+        return 0;
 }
