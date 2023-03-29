c2419797131ee90071f7ef0121048c37e58602cf
akm-akm <adityanick08@gmail.com>
2023-03-29 14:46:37 +0530

    .

--- hashmap/use.cpp
+++ hashmap/use.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
-#include<unordered_map>
+#include <unordered_map>
 using namespace std;
 
-int main(){
-    unordered_map <string,int> m;
+int main()
+{
+    unordered_map<string, int> m;
 
-    pair <string,int> a("hello",1);
+    pair<string, int> a("hello", 1);
     m.insert(a);
-    pair <string,int> p = make_pair("how",2);
+    pair<string, int> p = make_pair("how", 2);
     m.insert(p);
     cout << m.at("hello") << endl;
     cout << m.at("how") << endl;
@@ -16,14 +17,15 @@ int main(){
 
     cout << m["ok"] << endl;
     cout << m.at("ok") << endl;
-    for(auto x:m){
-        cout<<x.first<<" "<<x.second<<endl;
-        
+    for (auto x : m)
+    {
+        cout << x.first << " " << x.second << endl;
     }
-    unordered_map<string, int> :: iterator i = m.begin();
-    while(i!=m.end()){
+    unordered_map<string, int>::iterator i = m.begin();
+    while (i != m.end())
+    {
         cout << i->first << i->second << endl;
         i++;
     }
-        return 0;
+    return 0;
 }
